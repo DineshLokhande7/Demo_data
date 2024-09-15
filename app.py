dc33dd6d5e8aff8285f84a8ed8f76c019a0612ae
DineshLokhande7 <dinulokhande1995@gmail.com>
2024-09-15 14:13:31 +0530

    Create app.py
    Its a sample webpage, made by using python

---
+++ app.py
@@ -0,0 +1,11 @@
+import streamlit as st
+
+# Find more emojis here: https://www.webfx.com/tools/emoji-cheat-sheet.
+
+st.set_page_config(page_title="My Webpage", page_icon=":tada:", layout="wide")
+
+# ---------HEADER SECTION---------
+
+st.subheader("Hi, I am Dinesh :wave:")
+st.title("A DevOps engineer From India")
+st.write("I am passionate about finding ways to use Python and linux to be more efficient and effective in DevOps Journey")
